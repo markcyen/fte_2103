5361d9b4d10c30493fa17ded336b1ab99643ba7d
MarkY <markcyen@yahoo.com>
2021-04-20 14:09:24 -0600

    Create initialize test

---
+++ spec/event_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+RSpec.describe Event do
+  context '#initialize' do
+    it 'exists' do
+      event = Event.new("South Pearl Street Farmers Market")
+
+      expect(event).to be_instance_of(Event)
+    end
+
+    it 'has attributes' do
+      event = Event.new("South Pearl Street Farmers Market")
+
+      expect(event.name).to eq("South Pearl Street Farmers Market")
+      expect(event.food_trucks).to eq([])
+    end
+  end
+end
