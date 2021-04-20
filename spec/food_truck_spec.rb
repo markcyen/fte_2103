ce04c301ec1b0d0cd6a21d1b43d73636dbd25311
MarkY <markcyen@yahoo.com>
2021-04-20 13:53:59 -0600

    Modify stock test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -37,5 +37,17 @@ RSpec.describe Item do
       food_truck.stock(item1, 30)
       expect(food_truck.check_stock(item1)).to eq(30)
     end
+
+    it 'stock item and check for incremental add to stock' do
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+      food_truck.stock(item1, 30)
+      expect(food_truck.check_stock(item1)).to eq(30)
+
+      food_truck.stock(item1, 25)
+      expect(food_truck.check_stock(item1)).to eq(55)
+    end
   end
 end
