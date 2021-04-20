a7773a22ea764396168ef9986cf7bbe5bb2b6173
MarkY <markcyen@yahoo.com>
2021-04-20 15:05:12 -0600

    Commit final work

--- lib/event.rb
+++ lib/event.rb
@@ -22,4 +22,19 @@ class Event
       food_truck.inventory.include?(item)
     end
   end
+
+  # def total_inventory
+  #   items_hash = Hash.new { |hash, key| hash[key] = {} }
+  #   @food_trucks.each do |food_truck|
+  #     food_truck.each do |inventory|
+  #       inventory.items
+  # end
+
+  # def overstocked_items
+  #   @food_trucks.find_all do |food_truck|
+  #     food_truck.inventory.sum do |value|
+  #       inventory.value > 50
+  #     end
+  #   end
+  # end
 end
