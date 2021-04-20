3ed01f68425e75b31e77e0af9430dc6b3b98e3a0
MarkY <markcyen@yahoo.com>
2021-04-20 14:05:02 -0600

    Modify test to add second item

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -46,13 +46,21 @@ RSpec.describe Item do
       food_truck.stock(item1, 30)
       expect(food_truck.check_stock(item1)).to eq(30)
 
-      expected = {
+      expected_1 = {
         item1 => 30
       }
-      expect(food_truck.inventory).to eq(expected)
+      expect(food_truck.inventory).to eq(expected_1)
 
       food_truck.stock(item1, 25)
       expect(food_truck.check_stock(item1)).to eq(55)
+
+      food_truck.stock(item2, 12)
+      expected_2 = {
+        item1 => 55,
+        item2 => 12
+      }
+      expect(food_truck.inventory).to eq(expected_2)
+
     end
   end
 end
