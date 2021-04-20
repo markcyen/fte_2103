879a37fbf612d9434df7354eeb0aaa2c8d6bf8e4
MarkY <markcyen@yahoo.com>
2021-04-20 13:24:01 -0600

    Make minor change to item test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,13 +4,13 @@ require './lib/item'
 RSpec.describe Item do
   context '#initialize' do
     it 'exists' do
-      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})
 
       expect(item1).to be_instance_of(Item)
     end
 
     it 'has attributes' do
-      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})
       item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
       expect(item2.name).to eq("Apple Pie (Slice)")
