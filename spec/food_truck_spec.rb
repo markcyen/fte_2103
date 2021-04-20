9159fada2e6caf694d0d2658c100a4b0a75c0c76
MarkY <markcyen@yahoo.com>
2021-04-20 13:28:20 -0600

    Create test for check stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -17,4 +17,14 @@ RSpec.describe Item do
       expect(food_truck.inventory).to eq({})
     end
   end
+
+  context '#check_stock' do
+    it 'check stock for an item' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: '$3.75'})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+      expect(food_truck.check_stock(item1)).to eq(0)
+    end
+  end
 end
