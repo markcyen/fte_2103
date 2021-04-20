d58412ed3cb0e1f665c7bee09e579deb51871792
MarkY <markcyen@yahoo.com>
2021-04-20 13:54:18 -0600

    Modify implementation of stock method

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,10 +1,12 @@
 class FoodTruck
   attr_reader :name,
-              :inventory
+              :inventory,
+              :incremental_amount
 
   def initialize(name)
     @name = name
     @inventory = Hash.new { |hash, key| hash[key] = 0 }
+    @incremental_amount = 0
   end
 
   def check_stock(item)
@@ -13,5 +15,7 @@ class FoodTruck
 
   def stock(item, amount)
     @inventory[item] = amount
+    @incremental_amount += amount
+    @inventory[item] = @incremental_amount
   end
 end
